398a9d0c774f05ddb9ab8cf750c15de324fc9147
Dmytro Morozov <dmytro.morozov@microsoft.com>
2021-11-08 18:41:03 +0100

    Update packages and usages

--- AzureImageBuilder.cs
+++ AzureImageBuilder.cs
@@ -1,9 +1,7 @@
-using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
+using Azure.Messaging.EventGrid;
 
 namespace AzureImageBuilderFunctionApp
 {
